d183beb6bf7619549e72dedc2553a87b9f6bbc5f
softshaile <shailendrasthakur@gmail.com>
2020-01-17 14:44:35 +0000

    Addition of codebase to feature branch

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          This is  new .. There.
+          This is related to feature branch.
         </p>
         <a
           className="App-link"
